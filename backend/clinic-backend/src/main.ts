d106301dc567616c4b1c4d8f3d7d0b9a8ccca89e
Kushal J <kushalj400231@gmail.com>
2025-08-08 17:42:21 +0530

    Update backend url

--- backend/clinic-backend/src/main.ts
+++ backend/clinic-backend/src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); 
+
+  app.enableCors({
+    origin: ['http://localhost:3000','https://clinic-front-desk-frontend.vercel.app'], // ✅ Your deployed frontend URL
+    credentials: true, // ✅ Needed if you're using Authorization headers (like JWT tokens)
+  });
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
