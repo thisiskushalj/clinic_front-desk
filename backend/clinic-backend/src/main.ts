47bb2ab83f62592eb46023deb6dc0535fc1fa143
Kushal J <kushalj400231@gmail.com>
2025-08-08 17:57:42 +0530

    Origin true for test

--- backend/clinic-backend/src/main.ts
+++ backend/clinic-backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000','https://clinic-front-desk-frontend.vercel.app'], // ✅ Your deployed frontend URL
+    origin: true, // ✅ Your deployed frontend URL
     credentials: true, // ✅ Needed if you're using Authorization headers (like JWT tokens)
   });
 
